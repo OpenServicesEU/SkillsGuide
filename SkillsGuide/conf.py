b3c5c98c245acd9d2d363732ccf2bb94ebb46a82
Michael Fladischer <FladischerMichael@fladi.at>
2022-02-12 20:58:42 +0100

    Add LZK ClinicalTraineeshipsChecklist integration.

--- SkillsGuide/conf.py
+++ SkillsGuide/conf.py
@@ -4,6 +4,8 @@ from django.conf import settings
 
 class SkillsGuideAppConf(AppConf):
     COPYRIGHT = "Some Company"
+    LZK_API_URL = "https://lernzielkatalog-medizin.at/api"
+    LZK_API_CACHE_TIMEOUT = 3600
 
     class Meta:
         prefix = "SkillsGuide"
--- SkillsGuide/context_processors.py
+++ SkillsGuide/context_processors.py
@@ -1,3 +1,7 @@
+import requests
+from django.core.cache import cache
+from purl import URL
+
 from .conf import settings as app_settings
 from . import models
 
@@ -11,7 +15,34 @@ def settings(request):
 
 
 def navigation(request):
+    clinical_traineeship_checklist_key = f"{__name__}/clinical_traineeship_checklist"
+    clinical_traineeship_checklist = cache.get(clinical_traineeship_checklist_key)
+    if clinical_traineeship_checklist is None:
+        clinical_traineeship_checklist = dict()
+        u = URL(
+            app_settings.SKILLSGUIDE_LZK_API_URL
+        ).add_path_segment(
+            "skills"
+        ).append_query_param(
+            "clinical_traineeship_checklist",
+            "True"
+        ).as_string()
+        s = requests.Session()
+        s.headers.update({"Accept": "application/json"})
+        while u:
+            with requests.get(u, headers={"Accept": "application/json"}) as resp:
+                data = resp.json()
+                clinical_traineeship_checklist.update(
+                    {obj.get("target"): obj.get("name") for obj in data.get("results")}
+                )
+                u = data.get("next")
+        cache.set(
+            clinical_traineeship_checklist_key,
+            clinical_traineeship_checklist,
+            app_settings.SKILLSGUIDE_LZK_API_CACHE_TIMEOUT
+        )
     return {
         "top_downloads": models.Download.objects.filter(active=True, top=True),
         "chapters": models.Chapter.objects.all(),
+        "clinical_traineeship_checklist": clinical_traineeship_checklist,
     }
